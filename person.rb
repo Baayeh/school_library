5fd0a2740ac66d3e1c8f42ad14a31e654a2e22f1
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-12-19 14:44:40 +0000

    Add Person class

---
+++ person.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(_age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    true if is_of_age? || @parent_permission
+  end
+end
