8d5110b8a449296dec8317593e0e1565c77e6190
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-12-21 02:15:19 +0000

    Make Person class inherit from Nameable

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
@@ -17,5 +19,9 @@ class Person
 
   def can_use_services?
     true if of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
