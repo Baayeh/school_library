a885c76f16c124ebacd80841dc6e22947793e21e
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-12-21 13:58:58 +0000

    Create method to list books

---
+++ app.rb
@@ -0,0 +1,58 @@
+class App
+
+  attr_accessor :books, :people, :rentals
+
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+  end
+
+  def run
+    puts menu
+  end
+
+  # List App Menu
+  def menu
+    begin
+      puts "Welcome to School Library App!"
+      puts ''
+      puts 'Please choose an option by entering a number:'
+      puts '1 - List all books'
+      puts '2 - List all people'
+      puts '3 - Create a person'
+      puts '4 - Create a book'
+      puts '5 - Create a rental'
+      puts '6 - List all rentals for a given person id'
+      puts '7 - Exit'
+      puts ''
+      user_input = gets.chomp.to_i
+      puts ''
+
+      case user_input
+      when 1..6 then run_option(user_input)
+      when 7 then puts 'Thank you for using the app!'
+      else puts 'Please enter valid option'
+      end
+    end while user_input != 7
+  end
+
+  def run_option(user_input)
+    case user_input
+      when 1
+        puts 'List of books'
+        puts '-' * 13
+        list_books
+        puts ''
+      
+    end
+  end
+
+  def list_books
+    puts 'No books available' if @books.empty?
+    @books.each do |book|
+      puts "Title: #{book.title}, Author: #{book.author}"
+    end
+  end
+
+end
