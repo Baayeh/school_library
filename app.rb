3fdd6d3639b2f7767523d638bfc0fbf20da36ee5
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-02 15:02:00 +0000

    Move methods to create a person to a separate file

--- app.rb
+++ app.rb
@@ -17,62 +17,6 @@ class App
     puts menu
   end
 
-  def list_books
-    if @books.empty?
-      puts 'No books available'
-    else
-      @books.each do |book|
-        puts "Title: #{book.title}, Author: #{book.author}"
-      end
-    end
-  end
-
-  def list_people
-    if @people.empty?
-      puts 'No person available'
-    else
-      @people.each do |person|
-        puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-      end
-    end
-  end
-
-  def create_person
-    print 'Do you want to create a student (1) or a teacher (2)?: '
-    user_input = gets.chomp.to_i
-    case user_input
-    when 1
-      create_student
-    when 2
-      create_teacher
-    else puts 'Please select 1 or 2'
-    end
-  end
-
-  def create_student
-    print 'Age: '
-    age = gets.chomp.to_i
-    print 'Name: '
-    name = gets.chomp
-    print 'Does student have parent permission? [Y/N]: '
-    permission = gets.chomp
-    new_student = Student.new(age, permission, name)
-    @people.push(new_student)
-    puts 'Student created successfully'
-  end
-
-  def create_teacher
-    print 'Age: '
-    age = gets.chomp.to_i
-    print 'Name: '
-    name = gets.chomp
-    print 'Specialization: '
-    specialization = gets.chomp
-    teacher = Teacher.new(age, specialization, name)
-    @people.push(teacher)
-    puts 'Teacher created successfullly'
-  end
-
   def create_book
     print 'Title: '
     title = gets.chomp
--- lib_methods.rb
+++ lib_methods.rb
@@ -23,4 +23,40 @@ module Actions
       end
     end
   end
+
+  def create_person
+    print 'Do you want to create a student (1) or a teacher (2)?: '
+    user_input = gets.chomp.to_i
+    case user_input
+    when 1
+      create_student
+    when 2
+      create_teacher
+    else puts 'Please select 1 or 2'
+    end
+  end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Does student have parent permission? [Y/N]: '
+    permission = gets.chomp
+    new_student = Student.new(age, permission, name)
+    @people.push(new_student)
+    puts 'Student created successfully'
+  end
+
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+    teacher = Teacher.new(age, specialization, name)
+    @people.push(teacher)
+    puts 'Teacher created successfullly'
+  end
 end
