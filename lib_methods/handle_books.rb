a447569cfd2460601fefab08211e77f7fcfb1f44
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-01-03 15:29:30 +0100

    Add save_book method

--- lib_methods/handle_books.rb
+++ lib_methods/handle_books.rb
@@ -1,4 +1,5 @@
 require './book'
+require 'json'
 
 module HandleBook
   def list_books
@@ -18,6 +19,24 @@ module HandleBook
     author = gets.chomp
     book = Book.new(title, author)
     @books.push(book)
+    save_book(book)
     puts 'Book created successfullly'
   end
+
+  # Saving books
+  def save_book(book)
+    if(File.exists?('./book.json'))
+      file = File.read('./book.json')
+      data = JSON.parse(file)
+      data << book
+      data_json = data.to_json
+      File.open('./book.json', 'w') do |file|
+        file.puts data_json
+      end
+    else
+      File.open('./book.json', 'w') do |file|
+        file.puts @books.to_json
+      end
+    end
+  end   
 end
